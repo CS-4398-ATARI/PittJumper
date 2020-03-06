feb539ffaf411ea0d03810b60efba0eae2932ebf
Chad Palmer <chad.n.palmer@gmail.com>
2020-03-06 12:30:56 -0600

    renamed test file

--- unitTesting.py
+++
@@ -1,7 +0,0 @@
-#Test 1a and 1b: Validate user moves left and right as expected.
-
-#Test 2: Confirm that avitar jumps when initiated.
-
-#Test 3: Confirm that HP modifies when attacked.
-
-#Test 4: Validate game clock runs, pauses and stops as expected.
