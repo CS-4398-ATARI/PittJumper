24444ecb829a2f33839e517e2c8f1b87e18fcf21
Chad Palmer <chad.n.palmer@gmail.com>
2020-03-06 12:26:31 -0600

    unittests skeleton created

---
+++ test_game.py
@@ -0,0 +1,20 @@
+import unittest
+import unittest.mock
+import main
+import sprites
+
+class TestCases(unittest.TestCase):
+
+    #Test 1a and 1b: Validate user moves left and right as expected.
+
+    #Test 2: Confirm that avitar jumps when initiated.
+
+    #Test 3: Confirm that HP modifies when attacked.
+
+    #Test 4: Validate game clock runs, pauses and stops as expected.
+
+
+# This allows test_game.py to be executed directly without having
+# to run unittest as main module with test_game as argument
+if __name__ == '__main__':
+    unittest.main()
